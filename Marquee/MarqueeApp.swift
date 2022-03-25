269829f649884e87e9770f62dac500919cdb8d1d
Tal Spektor <talspektor@gmail.com>
2022-03-25 23:12:24 +0300

    Initial Commit

---
+++ Marquee/MarqueeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarqueeApp.swift
+//  Marquee
+//
+//  Created by Tal talspektor on 25/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct MarqueeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
